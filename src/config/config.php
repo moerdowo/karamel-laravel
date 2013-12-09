cf76af2260d5ad040eef3587f717d48bb624b08b
Jarvis Store <info@jarvis-store.com>
2013-12-09 19:41:00 +0700

    empty config

--- src/config/config.php
+++ src/config/config.php
@@ -2,8 +2,7 @@
 
 return array(
 
-    'dev' => 'totox777',
-    'apikey'  => 'ff5f46123fc60065c4dc0511d5906c6d',  // Set your Public Key here
-	'memberid'  => 'rf.moerdowo@gmail.com'  // Set your user ID (email) here
+    'apikey'  => '',  // Set your Public Key here
+	'memberid'  => ''  // Set your user ID (email) here
 
 );
